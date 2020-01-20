f234bbe348c1edb5b1447e2487294c79b7e12ac5
Luis Preza <preza.luis@gmail.com>
2020-01-20 14:49:42 -0600

    book constructor

---
+++ app.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+function addBookToLibrary(book) {
+  return myLibrary.push(book);
+}
