59295785c1378a11d02cad259bd3face52be9cb1
J. Christian Romero <jcromerohdz@gmail.com>
2020-01-20 13:17:30 -0800

    Reder function for miLibrary items

--- app.js
+++ app.js
@@ -10,3 +10,34 @@ function Book(title, author, pages, status) {
 function addBookToLibrary(book) {
   return myLibrary.push(book);
 }
+
+function render() {
+    let tbody = document.getElementById("body");
+    let tr = document.createElement("tr");
+    for (let book in myLibrary) {
+    
+        cell1 = document.createElement("td");
+        node1 = document.createTextNode(myLibrary[book].title);
+        cell2 = document.createElement("td");
+        node2 = document.createTextNode(myLibrary[book].author);
+        cell3 = document.createElement("td");
+        node3 = document.createTextNode(myLibrary[book].pages);
+        cell4 = document.createElement("td");
+        node4 = document.createTextNode(myLibrary[book].status);
+        cell1.appendChild(node1);
+        cell2.appendChild(node2);
+        cell3.appendChild(node3);
+        cell4.appendChild(node4);
+        tr.appendChild(cell1);
+        tr.appendChild(cell2);
+        tr.appendChild(cell3);
+        tr.appendChild(cell4);
+        tbody.appendChild(tr);
+    }
+}
+
+book = new Book("test", "hello", "blha", "1");
+
+addBookToLibrary(book);
+
+render();
